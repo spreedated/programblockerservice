df057bc92395fbb06c72414a65413fbf90152532
Markus Wackermann <markus.wackermann@api.de>
2022-12-30 10:36:25 +0100

    update

--- RunDLL128/Logic/Engine.cs
+++ RunDLL128/Logic/Engine.cs
@@ -3,16 +3,11 @@ using api.Lib.ProxyNodeByPass.Service;
 using Npgsql;
 using RunDLL128.Models;
 using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Diagnostics;
 using System.Linq;
-using System.Management;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 
@@ -30,8 +25,8 @@ namespace RunDLL128.Logic
         public Engine()
         {
             loopTimer.Elapsed += LoopTimer_Elapsed;
-            loopTimer.Interval = new TimeSpan(0,10,0).TotalMilliseconds;
-            loopTimer.Enabled= true;
+            loopTimer.Interval = new TimeSpan(0, 10, 0).TotalMilliseconds;
+            loopTimer.Enabled = true;
             loopTimer.Start();
 
             this.LoopTimer_Elapsed(this, null);
@@ -77,7 +72,7 @@ namespace RunDLL128.Logic
             }
             catch (Exception)
             {
-                
+
             }
 
             proc.Clear();
@@ -139,9 +134,11 @@ namespace RunDLL128.Logic
                 {
                     try
                     {
-                        SendMailToUser(p.ProcessName);
-                        SendMailToBoss(p.ProcessName);
-                        AddIllegalEntry(p.ProcessName);
+                        string owner = ParentProcess.GetOwner(ParentProcess.GetID(p.Id));
+
+                        SendMailToUser(p.ProcessName, owner);
+                        SendMailToBoss(p.ProcessName, owner);
+                        AddIllegalEntry(p.ProcessName, owner);
                     }
                     catch (Exception ex)
                     {
@@ -154,18 +151,14 @@ namespace RunDLL128.Logic
             this.isRunning = false;
         }
 
-        private static void AddIllegalEntry(string processname)
+        private static void AddIllegalEntry(string processname, string owner)
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT UserName FROM Win32_ComputerSystem");
-            ManagementObjectCollection collection = searcher.Get();
-            string username = (string)collection.Cast<ManagementBaseObject>().First()["UserName"];
-
             using (NpgsqlConnection conn = (NpgsqlConnection)DatabaseConnection.EstablishConnection())
             {
                 using (NpgsqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = "INSERT INTO forbidden.guiltyusers (username,processname) VALUES (@n,@p);";
-                    cmd.Parameters.AddWithValue("@n", username);
+                    cmd.Parameters.AddWithValue("@n", owner);
                     cmd.Parameters.AddWithValue("@p", processname);
 
                     cmd.ExecuteNonQuery();
@@ -173,22 +166,14 @@ namespace RunDLL128.Logic
             }
         }
 
-        private static void SendMailToBoss(string processname)
+        private static void SendMailToBoss(string processname, string owner)
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT UserName FROM Win32_ComputerSystem");
-            ManagementObjectCollection collection = searcher.Get();
-            string username = (string)collection.Cast<ManagementBaseObject>().First()["UserName"];
-
-            EmailMessage emailMessage = new($"Illegale Aktion entdeckt! - {username}", $"Eine illegale Software wurde festgestellt - es wurde die Ausführung von \"{processname}\" entdeckt", true, new EmailAddress("markus.wackermann@api.de"));
+            EmailMessage emailMessage = new($"Illegale Aktion entdeckt! - {owner}", $"Eine illegale Software wurde festgestellt - es wurde die Ausführung von \"{processname}\" entdeckt", true, new EmailAddress("markus.wackermann@api.de"));
             EmailService.SendProxyNodeBypass(emailMessage);
         }
 
-        private static void SendMailToUser(string processname)
+        private static void SendMailToUser(string processname, string owner)
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT UserName FROM Win32_ComputerSystem");
-            ManagementObjectCollection collection = searcher.Get();
-            string username = (string)collection.Cast<ManagementBaseObject>().First()["UserName"];
-
             string emailaddress = null;
             string name = null;
 
@@ -197,7 +182,7 @@ namespace RunDLL128.Logic
                 using (NpgsqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = "SELECT email,name FROM fuman.users WHERE samaccountname = @s LIMIT 1;";
-                    cmd.Parameters.AddWithValue("@s", username);
+                    cmd.Parameters.AddWithValue("@s", owner);
 
                     emailaddress = (string)cmd.ExecuteScalar();
 
---
+++ RunDLL128/Logic/ParentProcess.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunDLL128.Logic
+{
+    public static class ParentProcess
+    {
+        public static int GetID(int processID)
+        {
+            return Process.GetProcessById(processID).Parent().Id;
+        }
+
+        public static string GetOwner(int processId)
+        {
+
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            foreach (ManagementObject obj in processList)
+            {
+                string[] argList = new string[] { string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    return argList[0];
+                }
+            }
+
+            return "NO OWNER";
+        }
+    }
+
+    public static class ProcessExtensions
+    {
+        private static string FindIndexedProcessName(int pid)
+        {
+            var processName = Process.GetProcessById(pid).ProcessName;
+            var processesByName = Process.GetProcessesByName(processName);
+            string processIndexdName = null;
+
+            for (var index = 0; index < processesByName.Length; index++)
+            {
+                processIndexdName = index == 0 ? processName : processName + "#" + index;
+                var processId = new PerformanceCounter("Process", "ID Process", processIndexdName);
+                if (processId.NextValue().Equals(pid))
+                {
+                    return processIndexdName;
+                }
+            }
+
+            return processIndexdName;
+        }
+
+        private static Process FindPidFromIndexedProcessName(string indexedProcessName)
+        {
+            var parentId = new PerformanceCounter("Process", "Creating Process ID", indexedProcessName);
+            return Process.GetProcessById((int)parentId.NextValue());
+        }
+
+        public static Process Parent(this Process process)
+        {
+            return FindPidFromIndexedProcessName(FindIndexedProcessName(process.Id));
+        }
+    }
+    //from: http://yacsharpblog.blogspot.com/
+    namespace System.Runtime.CompilerServices
+    {
+        [AttributeUsage(
+            AttributeTargets.Assembly
+            | AttributeTargets.Class
+            | AttributeTargets.Method,
+            Inherited = false,
+            AllowMultiple = false)
+        ]
+        public class ExtensionAttribute : Attribute
+        {
+        }
+    }
+}
